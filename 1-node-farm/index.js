a90e45c3d7262a1b5aee4164c6f5c85ba3fde720
Cristian Herrera <cristiandherrera95@gmail.com>
2021-09-15 00:26:04 -0700

    Reading and writing files asynchronously

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,11 +1,43 @@
 const fs = require('fs')
 
+// // **************************************
+// // Reading and Writing Files Sync & Async
+// // **************************************
 
-// Blocking synchronous way
-const text = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(text)
+// // BLOCKING SYNCHRONOUSLY
+// const text = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(text)
 
-const textOut = `This is what we know about avocados: ${text}. \nCreated on ${Date.now()}`
+// const textOut = `This is what we know about avocados: ${text}. \nCreated on ${Date.now()}`
 
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log("File Written!");
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log("File Written!");
+
+// // NON-BLOCKING ASYNCHRONOUSLY - BUT "CALLBACK HELL"
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       fs.writeFile('./txt/conjoined.txt', `${data2}\n${data3}`, (err) => {
+//         if (err) throw err;
+//         console.log('CALL BACK HELL COMPLETE!');
+//       })
+//     });
+//   });
+// });
+// console.log('Waiting...');
+
+// // GOODBYE HELL!
+// const fCallBackHell = async function () {
+//   const data1 = fs.readFileSync('./txt/start.txt', 'utf-8')
+//   const data2 = fs.readFileSync(`./txt/${data1}.txt`, 'utf-8')
+//   const data3 = fs.readFileSync('./txt/append.txt', 'utf-8')
+
+//   fs.writeFile('./txt/conjoined.txt', `${data2}\n${data3}`, (err) => {
+//     if (err) throw err;
+//     console.log('CALL BACK HELL COMPLETE!');
+//   })
+// }
+// console.log('Waiting...');
+// fuckCallBackHell();
+
+
